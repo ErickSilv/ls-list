ea5035e40de6b6afd40b6244e5b81331206a2842
Daniel Barbosa <danielmnscb@gmail.com>
2017-06-03 22:05:16 -0300

    Modification on list.cpp

---
+++ src/driver.cpp
@@ -0,0 +1,18 @@
+/* Linguagens de Programação 2017.1
+ * Autorship: Daniel Barbosa and Erick Silva
+ * 
+ * Implementation of the List ADT using doubly linked list
+*/
+
+// Quick compilation
+// g++ src/list.cpp src/driver.cpp -o bin/list -I include -Wall -std=c++11 
+
+#include <iostream>
+
+#include "list.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- src/list.cpp
+++ src/list.cpp
@@ -1,9 +1,120 @@
+/* Linguagens de Programação 2017.1
+ * Autorship: Daniel Barbosa and Erick Silva
+ * 
+ * Implementation of the List ADT using doubly linked list
+*/
+
 #include <iostream>
 
 #include "list.h"
 
-int main()
+using namespace ls;
+
+// =========== CONST_ITERATOR THINGS ===========
+
+// >> const_iterator operators
+// Operator* (*const_iterator)
+template <typename T>
+const T & list<T>::const_iterator::operator*() const
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    
 }
+
+// Operator++ (++const_iterator)
+template <typename T>
+typename list<T>::const_iterator & ls::list<T>::const_iterator::operator++()
+{
+    
+}
+
+// Operator++ (const_iterator++)
+template <typename T>
+typename list<T>::const_iterator ls::list<T>::const_iterator::operator++( int )
+{
+    
+}
+
+// Operator-- (--const_iterator)
+template <typename T>
+typename list<T>::const_iterator & ls::list<T>::const_iterator::operator--()
+{
+    
+}
+
+// Operator-- (const_iterator--)
+template <typename T>
+typename list<T>::const_iterator ls::list<T>::const_iterator::operator--( int )
+{
+    
+}
+
+// Operator==
+template <typename T>
+bool list<T>::const_iterator::operator== ( const const_iterator & rhs ) const
+{
+    // STUB IMPLEMENTATION
+    return true;
+}
+
+// Operator!=
+template <typename T>
+bool list<T>::const_iterator::operator!= ( const const_iterator & rhs ) const
+{
+    // STUB IMPLEMENTATION
+    return true;
+}
+
+// =========== ITERATOR THINGS ===========
+
+// >> iterator operators
+// Operator* (*iterator)
+template <typename T>
+T & list<T>::iterator::operator*()
+{
+    
+}
+
+// Operator++ (++iterator)
+template <typename T>
+typename list<T>::iterator & list<T>::iterator::operator++()
+{
+    
+}
+
+// Operator++ (iterator++)
+template <typename T>
+typename list<T>::iterator list<T>::iterator::operator++( int )
+{
+    
+}
+
+// Operator-- (--iterator)
+template <typename T>
+typename list<T>::iterator & list<T>::iterator::operator--()
+{
+    
+}
+
+// Operator-- (iterator--)
+template <typename T>
+typename list<T>::iterator list<T>::iterator::operator--( int )
+{
+    
+}
+
+// Operator==
+template <typename T>
+bool list<T>::iterator::operator== ( iterator & rhs ) const
+{
+    // STUB IMPLEMENTATION
+    return true;
+}
+
+// Operator!=
+template <typename T>
+bool list<T>::iterator::operator!= ( iterator & rhs ) const
+{
+    // STUB IMPLEMENTATION
+    return true;
+}
+

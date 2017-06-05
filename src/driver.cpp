52f538fa0c4f12da497639117bc1777a0e513445
Eirkc de oliveira silva <erickos@A202-03L.imd.local>
2017-06-05 17:48:16 -0300

    Initial tests implementations

--- src/driver.cpp
+++ src/driver.cpp
@@ -14,8 +14,26 @@
 
 using namespace ls;
 
+template <typename T>
+void print( list<T> & v )
+{
+	list<T>::const_iterator b_it = v.begin();
+	list<T>::const_iterator e_it = v.end();
+
+	std::cout << "[ ";
+	for ( /*empty*/; b_it != e_it; ++b_it )
+		std::cout << *b_it << " ";
+	std::cout << "]\n";
+}
+
 int main()
 {
+	// Teste construtor vazio. //
+	list<int> listaVazia;
+	std::cout << "Lista Vazia" << std::endl;
+	print( listaVazia );
+
+
     // Criando nova lista vazia
     list<int> listaInteiros = { 2 , 5 , 7 , 8 , 9 , 11 , 15 };
 
